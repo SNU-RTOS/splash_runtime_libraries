0d99bb7e5864ca123ab458963d36e0ed5f1a7e31
khc196 <khc146@gmail.com>
2020-08-21 16:05:39 +0900

    Implement mode manager

--- srl/mode_manager.py
+++ srl/mode_manager.py
@@ -1,6 +1,6 @@
 from rclpy.node import Node
 import json
-from test_pkg_interfaces.srv import RegisterMode, RequestModeChange
+from splash_interfaces.srv import RegisterMode, RequestModeChange
 from std_msgs.msg import String
 
 class ModeManager(Node):
@@ -14,24 +14,31 @@ class ModeManager(Node):
         
     def _request_mode_change_callback(self, request, response):
         response.ok = False
-        cur_mode = next((item for item in self.mode_conf_map[request.factory]["mode_list"] if item["name"] == self.mode_map["factory"]), None)
+        cur_mode = next((item for item in self.mode_conf_map[request.factory]["mode_list"] if item["name"] == self.mode_map[request.factory]), None)
         if cur_mode:
             cur_event = next((item for item in cur_mode["events"] if item["name"] == request.event), None)
             if cur_event:
                 next_mode = cur_event["next_mode"]
                 response.ok = True
                 self.mode_map[request.factory] = next_mode
-                msg = ModeChange()
-                msg.mode = next_mode
-                self.publisher_map[request.factory].publish(msg)
+                msg = String()
+                msg.data = next_mode
+                for publisher in self.publisher_map[request.factory]:
+                    publisher.publish(msg)
 
         return response
 
     def _register_mode_callback(self, request, response):
-        response.ok = True
-
-        self.mode_conf_map[request.factory] = json.loads(request.mode_configuration)
-        self.mode_map[request.factory] = mode_conf_map[request.factory]["initial_mode"]
-        self.publisher_map[request.factory] = self.create_publisher(String, "splash_mode_change_{}".format(request.factory), 1)
-
+        print("Mode registration")
+        try:
+            response.ok = True
+            self.mode_conf_map[request.factory] = json.loads(request.mode_configuration)
+            self.mode_map[request.factory] = self.mode_conf_map[request.factory]["initial_mode"]
+            topic = "{}/splash_mode_change_{}".format(request.name_space, request.factory)
+            if not request.factory in self.publisher_map.keys():
+                self.publisher_map[request.factory] = []
+            self.publisher_map[request.factory].append(self.create_publisher(String, topic, 1))
+        except:
+            response.ok = False
+        
         return response
