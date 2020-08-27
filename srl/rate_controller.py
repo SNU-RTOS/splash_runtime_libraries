fe1ba424e2a4abd43be30a763a89133bf7b9f07c
khc196 <khc146@gmail.com>
2020-08-27 15:31:42 +0900

    Add time synchronization

--- srl/rate_controller.py
+++ srl/rate_controller.py
@@ -1,4 +1,4 @@
-
+from rclpy.time import Time
 
 class RateController():
     def __init__(self, stream_output_port):
@@ -6,20 +6,30 @@ class RateController():
         self._stream_output_port = stream_output_port
         self._publisher = self._stream_output_port.get_publisher()
         self._last_sent_item = None
-        self._create_timer()
+        self._initialized = False
+
     def _create_timer(self):
         component = self._stream_output_port.parent
-        component.create_timer(self._stream_output_port.get_rate_constraint(), self._task)
+        return component.create_timer(1.0/self._stream_output_port.get_rate_constraint(), self._task)
 
     def _task(self):
         if len(self._queue) > 0:
             msg = self._queue.pop(0)
+            time_diff_ms = (self._stream_output_port.parent.get_clock().now().nanoseconds - Time.from_msg(msg.header.stamp).nanoseconds) / 1000000
+            print(time_diff_ms)
+            if msg.freshness < time_diff_ms:
+                self._task()
+                return
         elif self._last_sent_item:
             msg = self._last_sent_item
         else:
             return
         self._last_sent_item = msg
-        self._publisher.publish(msg)
+        if self._stream_output_port.parent.mode == self._stream_output_port.parent.get_current_mode():
+            self._publisher.publish(msg)
     
     def push(self, msg):
+        if not self._initialized:
+            self._timer = self._create_timer()
+            self._initialized = True
         self._queue.append(msg)
